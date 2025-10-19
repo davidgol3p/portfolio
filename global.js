32301c2ac9397dda05b3f323ab9b7e4bd999b183
davidgol3p <dgoldstein@ucsd.edu>
2025-10-19 15:35:10 -0700

    hello

---
+++ global.js
@@ -0,0 +1,39 @@
+console.log('IT’S ALIVE!');
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// if (currentLink) {
+//   currentLink.classList.add("current");
+// }
+
+let pages = [
+    { url: '../', title: 'Home' },
+    { url: '../projects', title: 'Projects' },
+    { url: '../contact', title: 'Contact' },
+    { url: '../profile', title: 'Resume' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/"; 
+
+if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+}
